d3c6c8c772a455fe777087da0bccaa896e136fda
Ricardo Casares <ricardocasares@gmail.com>
2025-08-11 00:35:57 +0200

    libertango

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import Hydra from "hydra-synth";
 new Hydra({ detectAudio: true });
 
 if (import.meta.hot) {
-  import.meta.hot.accept("./song");
+  try {
+    import.meta.hot.accept("./song");
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 await import("./song");
--- src/songs/libertango.js
+++ src/songs/libertango.js
@@ -1,6 +1,6 @@
 v0("piazzolla");
 cut(0);
-smt(0);
+smt(0.1);
 bp(160);
 show(1);
 // rate0(1);
@@ -9,10 +9,10 @@ const o = osc(100).thresh().modulate(noise(tan)).rotate(t);
 
 src(s0)
   .saturate(0)
-  .color(ntan, ntan, tan)
+  .color(ntan, tan, ntan)
   .posterize(lo, lo)
-  // .mask(o)
+  // .modulateScale(osc(mid))
   // .pixelate(350, 350)
-  .mask(shape([2, 3, 4, 100]))
-  // .kaleid(0)
+  // .mask(shape([2, 3, 4, 100]))
+  // .modulateKaleid(noise(2).kaleid([1, 2, 3, 4, 10]))
   .out();
