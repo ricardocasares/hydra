8da626154c4f6eb62ffd2c83a63d0da70ee501aa
Ricardo Casares <ricardocasares@gmail.com>
2025-08-11 00:51:45 +0200

    libertango

--- src/songs/libertango.js
+++ src/songs/libertango.js
@@ -1,18 +1,20 @@
 v0("piazzolla");
-cut(0);
-smt(0.1);
+cut(-2);
+smt(0.3);
 bp(160);
 show(1);
-// rate0(1);
-
+// seek0(90); // bandoneon
+// seek0(397); // cara
 const o = osc(100).thresh().modulate(noise(tan)).rotate(t);
 
 src(s0)
   .saturate(0)
+  .colorama(mid)
   .color(ntan, tan, ntan)
   .posterize(lo, lo)
-  // .modulateScale(osc(mid))
+  // .modulateScale(osc(hi))
   // .pixelate(350, 350)
   // .mask(shape([2, 3, 4, 100]))
-  // .modulateKaleid(noise(2).kaleid([1, 2, 3, 4, 10]))
+  // .modulateKaleid(osc(0).kaleid(range(1, 100)))
+  // .rotate(t)
   .out();
