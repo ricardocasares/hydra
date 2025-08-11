c55cd44ccc57b78ed649bbb4d50e44f957e45c8e
Ricardo Casares <ricardocasares@gmail.com>
2025-08-11 03:02:51 +0200

    lejos

--- src/songs/lejos.js
+++ src/songs/lejos.js
@@ -2,16 +2,15 @@ v0("flashback");
 cut(3);
 smt(0.5);
 bp(120);
-cam0(0);
 
 src(s0)
   .saturate(0)
   .color(r, g, () => b() * -1)
   .mask(
-    shape([2, 3, 4, 100])
-      .scale(() => r() * 2)
-      .rotate(() => time)
+    shape([2, 3, 4, 884])
+      .scale(lo)
       .modulate(osc(Math.PI * 20, 2).thresh())
   )
   .kaleid([2, 50, 10])
+  .rotate(t)
   .out();
