e0ac09e0ec5c3b47cd8af60507b4b51757f4e6d8
Rick <ricardocasares@gmail.com>
2026-08-04 16:19:29 +0200

    peregrino

--- src/songs/07_peregrino.js
+++ src/songs/07_peregrino.js
@@ -4,10 +4,9 @@ show(1);
 cut(-2);
 smt(0.5)
 
-seek0(0)
+// seek0(20)
 
 src(s0)
-    // .invert()
     .saturate(0)
     .posterize(10, 1)
     .color([0,1,1].ease(), [1,0,1].ease(), [1,1,0].ease())
