786d1521b19037db823e4ab31e06c107c335d4bd
Rick <ricardocasares@gmail.com>
2026-08-04 17:11:55 +0200

    oblivion

--- src/song.js
+++ src/song.js
@@ -1,1 +1,1 @@
-import "./songs/07_peregrino";
+import "./songs/10_oblivion";
---
+++ src/songs/10_oblivion.js
@@ -0,0 +1,15 @@
+v0('oblivion');
+bp(88);
+show(1);
+cut(-2);
+smt(0)
+
+// seek0(0)
+
+src(s0)
+    .saturate(0)
+    .contrast(1)
+    .posterize(10, 1)
+    .color([0,1,1].ease(), [1,0,1].ease(), [1,1,0].ease())
+    // .scale([1,2,1,3])
+    .out();
