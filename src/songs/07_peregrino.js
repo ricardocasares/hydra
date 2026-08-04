306ca754711aaf7b900075917624fb360ca9bb91
Rick <ricardocasares@gmail.com>
2026-08-04 16:31:47 +0200

    p

--- src/songs/07_peregrino.js
+++ src/songs/07_peregrino.js
@@ -2,12 +2,13 @@ v0('peregrino');
 bp(120);
 show(1);
 cut(-2);
-smt(0.5)
+smt(0)
 
-// seek0(20)
+seek0(100)
 
 src(s0)
     .saturate(0)
+    .contrast(1)
     .posterize(10, 1)
     .color([0,1,1].ease(), [1,0,1].ease(), [1,1,0].ease())
     // .scale([1,2,1,3])
