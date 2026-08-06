09d4e235af32533481205f0841c85e08ac76c175
Rick <ricardocasares@gmail.com>
2026-08-06 13:14:59 +0200

    deto

--- src/songs/16_detonados.js
+++ src/songs/16_detonados.js
@@ -2,21 +2,17 @@ v0("climax");
 cut(0);
 smt(0.95);
 bp(120);
-
+show(1)
 // seek0(0)
 
 src(s0)
   .saturate(0)
-  .color(r, g, ntan)
+  // .invert()
+  .posterize(10,2)
+  .color([0,1,1].ease(), [1,0,1].ease(), [1,1,0].ease())
   .scale([1, 1.2, 0.8, 1.5])
   // .pixelate(hill(100, 300, 20), hill(100, 300, 20))
-  .mask(
-    shape([2, 3, 4, 100])
-      .scale(() => r() * 2)
-      .rotate(() => time)
-      .modulate(osc(Math.PI * 20, 2).thresh())
-  )
-  .kaleid([2, 5, 10, 20])
-  .rotate(t)
-  .posterize(10,4)
+  .mask(shape([2, 3, 4, 100]).scale(() => r() * 2).rotate(() => time).modulate(osc(Math.PI * 20, 2).thresh()))
+  // .kaleid([2, 5, 10, 20])
+  // .rotate(t)
   .out();
